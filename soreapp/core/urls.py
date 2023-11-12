624bd099fc14484ee99436d3a84a1f9c19d2b6ff
Filimon Dănuț-Dumitru <60850899+Danie83@users.noreply.github.com>
2023-11-12 19:53:17 +0200

    added login/register functionality

--- soreapp/core/urls.py
+++ soreapp/core/urls.py
@@ -3,6 +3,9 @@ from .views import *
 
 urlpatterns=[
     path('', IndexView.as_view(), name='index'),
+    path('login/', UserLoginView.as_view(), name='login'),
+    path('register/', UserRegistrationView.as_view(), name='register'),
+    path('logout/', UserLogoutView.as_view(), name='logout'),
     path('google-login/', GoogleLoginView.as_view(), name='google_login'),
     path('google-callback/', GoogleCallbackView.as_view(), name='google_callback'),
 ]
--- soreapp/core/views.py
+++ soreapp/core/views.py
@@ -1,15 +1,24 @@
-from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.views import  LogoutView
 from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
 from django.views import View
+from django.views.generic.edit import FormView
+
 from utils import googleoauth
-import requests
-import json
 
 # Create your views here.
 
-class IndexView(View):
+class IndexView(LoginRequiredMixin, View):
+    template_name = 'index.html'
+    login_url = 'login/'
+
     def get(self, request):
-        return render(request, 'index.html')
+        return render(request, self.template_name)
 
 class GoogleLoginView(View):
     def get(self, request):
@@ -24,3 +33,36 @@ class GoogleCallbackView(View):
             return HttpResponse("Authentication successful")
         else:
             return HttpResponse("Authentication failed")
+        
+class UserLoginView(View):
+    template_name = 'login.html'
+
+    def get(self, request, *args, **kwargs):
+        form = AuthenticationForm()
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request, *args, **kwargs):
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('index')
+        return render(request, self.template_name, {'form': form})
+
+class UserRegistrationView(View):
+    template_name = 'register.html'
+
+    def get(self, request, *args, **kwargs):
+        form = UserCreationForm()
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request, *args, **kwargs):
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('index')
+        return render(request, self.template_name, {'form': form})
+
+class UserLogoutView(LogoutView):
+    next_page = reverse_lazy('login')
